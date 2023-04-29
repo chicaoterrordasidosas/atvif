7524d75ee8164777956e5a9f376c2c3f561405a8
Francisco Barros <114003387+chicaoterrordasidosas@users.noreply.github.com>
2023-04-29 18:20:24 -0300

    Create q7.java

---
+++ q7.java
@@ -0,0 +1,23 @@
+public class QuestaoSete {
+    public static void main(String[] args) {
+        // Estrutura sequencial
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        
+        // Estrutura de repetição while
+        int i = 0;
+        while (i < 11) {
+            System.out.println("Hello World");
+            i++;
+        }
+    }
+}
